25c3017cf952c2b4a153e844dc6457f966fa2723
gllucass <geraldolucas10@gmail.com>
2024-07-20 23:23:14 -0300

    Resolvendo desafio Lista Tarefa

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import list.OperacoesBasicas.ListaTarefa;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ListaTarefa listaTarefa  = new ListaTarefa();
+        System.out.println("O numero Total de elemnto na lisat é: " + listaTarefa.obterNumeroTotalTarefas());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        listaTarefa.asicionarTarefa("Tarefa 1");
+        listaTarefa.asicionarTarefa("Tarefa 2");
+        listaTarefa.asicionarTarefa("Tarefa 3");
+
+        System.out.println("O numero Total de elemnto na lisat é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 2");
+
+        System.out.println("O numero Total de elemnto na lisat é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricaoTarefas();
     }
 }
